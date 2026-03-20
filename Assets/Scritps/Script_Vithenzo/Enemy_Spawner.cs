99143733ee06e77a953934ceda2b7e69360daf3f
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-03-19 22:08:47 -0300

    Update

---
+++ Assets/Scritps/Script_Vithenzo/Enemy_Spawner.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Enemy_Spawner : MonoBehaviour
+{
+    [Header("Config")]
+    public Transform[] pontoDeSpawn;
+    public GameObject[] objetosParaSpawnar;
+    public float tempoMaximoEntreOsSpwans;
+    public float TempoAtualDosSpwans;
+    internal static object instance;
+
+    public bool PodeSpawnar { get; private set; } = true;
+
+    void Start()
+    {
+        TempoAtualDosSpwans = tempoMaximoEntreOsSpwans;
+    }
+
+    void Update()
+    {
+        if (!PodeSpawnar) return;
+
+        TempoAtualDosSpwans -= Time.deltaTime;
+
+        if (TempoAtualDosSpwans <= 0)
+        {
+            SpawnarObjeto();
+        }
+    }
+
+    private void SpawnarObjeto()
+    {
+        int ObjetoAleatorio = Random.Range(0, objetosParaSpawnar.Length);
+        int pontoAleatorio = Random.Range(0, pontoDeSpawn.Length);
+
+        Instantiate(objetosParaSpawnar[ObjetoAleatorio],
+            pontoDeSpawn[pontoAleatorio].position,
+            Quaternion.Euler(0f, 0f, -90f));
+
+        TempoAtualDosSpwans = tempoMaximoEntreOsSpwans;
+    }
+
+    // NOVOS MÉTODOS:
+    public void PausarSpawns()
+    {
+        PodeSpawnar = false;
+    }
+
+    public void RetomarSpawns()
+    {
+        PodeSpawnar = true;
+        TempoAtualDosSpwans = tempoMaximoEntreOsSpwans;
+    }
+}
---
+++ Assets/Scritps/Script_Vithenzo/VidaDoPlayer.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class VidaDoPlayer : MonoBehaviour
+{
+    public int VidaMaximadoPlayer;
+
+    public int VidaatualDoPlayer;
+
+    public Slider BarradeVidaDoplayer;
+
+    public int vidaParaReceber;
+
+    public GameObject telaDerrota;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        VidaatualDoPlayer = VidaMaximadoPlayer;
+
+        BarradeVidaDoplayer.value = VidaatualDoPlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GanharVida(int VidaRecuperada)
+    {
+        if (VidaatualDoPlayer + VidaRecuperada <= VidaMaximadoPlayer)
+        {
+            VidaatualDoPlayer += VidaRecuperada;
+        }
+        else
+        {
+            VidaatualDoPlayer = VidaMaximadoPlayer;
+        }
+
+        BarradeVidaDoplayer.value = VidaatualDoPlayer;
+
+    }
+
+    public void ReiniciarJogo()
+    {
+        Time.timeScale = 1f; // despausa o jogo
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // recarrega a cena atual
+    }
+
+
+    public void DanoAoPlayer(int DanoParaReceber)
+    {
+
+        if (VidaatualDoPlayer <= 0)
+        {
+            if (telaDerrota != null)
+                telaDerrota.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
